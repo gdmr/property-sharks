32025bd7f6f37cce766363328499878d465b04ce
Gionata <gio.damore@hotmail.com>
2024-07-23 10:36:38 +0200

    modifiche classe prigione

--- include/prigione.hpp
+++ include/prigione.hpp
@@ -1,9 +1,15 @@
 #include "tessera.hpp"
+#include "giocatore.hpp"
 
 class Prigione : public Tessera
 {
 private:
+int giorniDiPrigione;
 
 public:
-
+Prigione(std::string nome, int giorni);
+~Prigione();
+void setGIorniDiPrigione(int giorni);
+int getGIorniDiPrigione();
+void vaiInPrigione(Giocatore g1);
 };
--- src/prigione.cpp
+++ src/prigione.cpp
@@ -1,1 +1,19 @@
 #include "prigione.hpp"
+#include "giocatore.hpp"
+
+Prigione::Prigione(std::string nome, int giorni) {
+    setTitolo(nome);
+    setGIorniDiPrigione(giorni);
+};
+Prigione::~Prigione(){};
+
+void Prigione::setGIorniDiPrigione(int giorni){
+    this->giorniDiPrigione=giorni;
+}
+int Prigione::getGIorniDiPrigione(){
+    return giorniDiPrigione;
+}
+
+void Prigione::vaiInPrigione(Giocatore g1){
+    g1.vaiInPrigione();
+}
